2f74e57866fc26009ed8c20c9a17f1188bdc3d92
Manuel Rossi <63568548+manurdls@users.noreply.github.com>
2020-06-05 22:50:28 -0300

    Add files via upload

---
+++ interface.py
@@ -0,0 +1,8 @@
+from zope.interface import Interface
+
+class IInterface(Interface):
+    def insertarElemento(posicion, componente):
+        pass
+
+    def agregarElementoFin(componente):
+        pass
